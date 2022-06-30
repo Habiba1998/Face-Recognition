1372e9a06e97714ef207eac27efd6d38257ce4ea
Habiba1998 <54662472+Habiba1998@users.noreply.github.com>
2022-06-30 08:11:24 -0700

    Update JSON request

--- API.py
+++ API.py
@@ -5,12 +5,13 @@ from PIL import Image
 import numpy as np
 import cv2
 from recognize import recognize_face
+import os
 
 app = Flask(__name__)
-
+#pipreqs --encoding utf-8 "./" --force
 @app.route('/', methods=['POST'])
 def predict():
-    payload = request.form.to_dict(flat=False)
+    payload = request.get_json()
     faces = []
     for i in range(len(payload['images'])):
         im_b64 = payload['images'][i]
@@ -19,16 +20,15 @@ def predict():
         img = Image.open(buf)
         faces.append(np.asarray(img))
 
-    captured_image_b64 = payload['image'][0]
+    captured_image_b64 = payload['image']
     captured_image_binary = base64.b64decode(captured_image_b64)
     buf = io.BytesIO(captured_image_binary)
     captured_image = cv2.cvtColor(np.array(Image.open(buf)), cv2.COLOR_RGB2BGR)
     dict = recognize_face(captured_image, faces)
-    id = payload['id'][dict['identity']]
+    id = None
+    if (dict['identity'] != None):
+        id = payload['id'][dict['identity']]
     return jsonify({'status' :str(dict['status']), 'id':str(id)})
 
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=12245)
+app.run(host='0.0.0.0', port='5000')
+    
